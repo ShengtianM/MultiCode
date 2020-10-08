96c6bdef60467cd531815f9d7baeecbba019419b
闵圣天 <gzutian@gmail.com>
2020-10-08 13:01:28 +0800

    feat：添加高德地图下载 URL

--- src/org/uniplore/http/DownloadPicFromURL.java
+++ src/org/uniplore/http/DownloadPicFromURL.java
@@ -21,7 +21,13 @@ public class DownloadPicFromURL {
 	private final static int Z=15;
 	private final static String baiduUrl = "http://online4.map.bdimg.com/tile/?qt=vtile&x=86&y=23&z=9&styles=pl&scaler=1&udt=20190815";
 	private final static String amapUrl = "https://wprd01.is.autonavi.com/appmaptile?x=26085&y=13886&z=15&lang=zh_cn&size=1&scl=2&style=8&ltype=11";
-   
+    private final static String url1 = "http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7"; //矢量图（含路网、含注记）
+    private final static String url2 = "http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=2&style=7"; //含路网，不含注记
+    private final static String url3 = "http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=6"; //影像底图（不含路网，不含注记））
+    private final static String url4 = "http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=2&style=6"; //影像底图（不含路网、不含注记）
+    private final static String url5 = "http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=8"; //影像路图（含路网，含注记）
+    private final static String url6 = "http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=2&style=8"; //影像路网（含路网，不含注记）
+      
 	
 	public static void main(String[] args) {
     	
